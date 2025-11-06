25324df48b9c26997239f2cb26c3d12b38432a90
TharaneshA <tharanesh2k5@gmail.com>
2025-11-06 21:44:42 +0530

    add: audio hparams

---
+++ voice_analyzer/__init__.py
@@ -0,0 +1,5 @@
+name = "voice_analyzer"
+
+from voice_analyzer.audio import preprocess_wav
+from voice_analyzer.hparams import sampling_rate
+from voice_analyzer.voice_encoder import VoiceEncoder
---
+++ voice_analyzer/hparams.py
@@ -0,0 +1,20 @@
+mel_window_length = 25
+mel_window_step = 10
+mel_n_channels = 40
+
+# Audio
+sampling_rate = 16000
+partials_n_frames = 160
+
+# Voice Activation Detection
+vad_window_length = 30
+vad_moving_average_width = 8
+vad_max_silence_length = 6
+
+# Audio volume normalization
+audio_norm_target_dBFS = -30
+
+# Model parameters
+model_hidden_size = 256
+model_embedding_size = 256
+model_num_layers = 3
